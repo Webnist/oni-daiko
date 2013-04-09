24e552ad44e860e136af7556d6d2af816ab45740
Webnist <webnist@webnist.org>
2013-04-09 18:56:15 +0900

    0.7.1.0

--- oni-daiko.php
+++ oni-daiko.php
@@ -6,8 +6,8 @@ Description: Shows a list of the latest posts from each blogs on a multisaite bl
 Version: 0.7.1.0
 Author: Webnist
 Author URI: http://www.webnist.net
-Network: true
 License: GPLv2 or later
+Network: true
 */
 
 if ( !defined( 'ONI_DAIKO_DIR' ) )
@@ -24,6 +24,7 @@ class OniDaiko {
 	private $plugin_dir;
 	private $plugin_url;
 	private $lang_path;
+	private $main_include = 1;
 	private $domain = 'oni-daiko';
 	private $slug = 'oni-daiko';
 	private $current_blog_id;
@@ -35,7 +36,8 @@ class OniDaiko {
 
 		load_plugin_textdomain( $this->domain, false, $this->lang_path );
 		$this->current_blog_id = get_current_blog_id();
-		$this->slug = get_option( 'oni-daiko-slug' );
+		$this->main_include = get_option( 'oni-daiko-main-include', 1 );
+		$this->slug = get_option( 'oni-daiko-slug', 'oni-daiko' );
 		if ( $this->current_blog_id == 1 ) {
 			register_activation_hook( __FILE__, array( &$this, 'flush_rewrite_rules', 'add_option' ) );
 			register_deactivation_hook( __FILE__, array( &$this, 'flush_rewrite_rules', 'delete_option' ) );
@@ -51,11 +53,14 @@ class OniDaiko {
 	
 			//add_action( 'loop_start', array( &$this, 'add_loop_start' ) );
 
-			add_action( 'the_post', array( &$this, 'add_the_post' ) );
+			//add_action( 'the_post', array( &$this, 'add_the_post' ) );
 			
-			add_action( 'loop_end', array( &$this, 'add_loop_end' ) );
+			//add_action( 'loop_end', array( &$this, 'add_loop_end' ) );
 
 			add_action( 'admin_menu', array( &$this, 'admin_menu' ) );
+			add_action( 'admin_init', array( &$this, 'add_general_custom_fields' ) );
+			add_filter( 'admin_init', array( &$this, 'add_custom_whitelist_options_fields' ) );
+			add_action( 'admin_print_styles', array( &$this, 'admin_styles' ) );
 		}
 	}
 
@@ -79,26 +84,26 @@ class OniDaiko {
 	public function add_rewrite_rule() {
 	
 		global $wp_rewrite;
-		$wp_rewrite->add_rewrite_tag( '%oni_daiko%', '(oni-daiko)', 'oni-daiko=' );
-		$wp_rewrite->add_permastruct( 'oni_daiko', '/%oni_daiko%/', false );
+		$wp_rewrite->add_rewrite_tag( '%' . $this->slug . '%', '(' . $this->slug . ')', $this->slug . '=' );
+		$wp_rewrite->add_permastruct( $this->slug, '%' . $this->slug . '%', false );
 
 	}
 
 	public function add_query_var( $vars ){
-		array_push( $vars, 'oni-daiko' );
+		array_push( $vars, $this->slug );
 		return $vars;
 	}
 
 	public function add_pre_get_posts( $query ) {
 		global $wp_query;
-		if ( !is_admin() && $query->is_main_query() && get_query_var( 'oni-daiko' ) && $this->current_blog_id == 1 ) {
+		if ( !is_admin() && $query->is_main_query() && get_query_var( $this->slug ) && $this->current_blog_id == 1 ) {
 			$query->is_home = false;
 		}
 	}
 	
 	public function add_posts_request( $sql, $query ) {
 		global $wpdb;
-		if( $query->is_main_query() && $this->current_blog_id == 1 && get_query_var( 'oni-daiko' ) == 'oni-daiko' ) {
+		if( $query->is_main_query() && $this->current_blog_id == 1 && get_query_var( $this->slug ) == $this->slug ) {
 			if ( preg_match('/(SELECT(.*))(FROM)/', $sql, $matches) ) {
 				$select = trim( $matches[1] );
 			}
@@ -136,13 +141,18 @@ class OniDaiko {
 
 	public function get_blog_list(){
 		global $wpdb;
-		$query = "SELECT * FROM {$wpdb->blogs} WHERE public = '1' ";
+
+		if ( $this->main_include != 1 )
+			$query = "SELECT * FROM {$wpdb->blogs} WHERE public = '1' AND blog_id != 1 ";
+		else
+			$query = "SELECT * FROM {$wpdb->blogs} WHERE public = '1' ";
+
 		$set_blog_list = $wpdb->get_results( $query );
 		return $set_blog_list;
 	}
 
 	public function add_loop_start( $query ) {
-		if ( !is_admin() && ($query->is_main_query()) && get_query_var( 'oni-daiko' ) ) {
+		if ( !is_admin() && $query->is_main_query() && get_query_var( $this->slug ) ) {
 			$posts = $query->posts;
 			foreach ( $posts as $post ) {
 				switch_to_blog( $post->blog_id );
@@ -152,23 +162,63 @@ class OniDaiko {
 	
 	public function add_the_post( $post ) {
 		global $wp_query;
-		if ( !is_admin() && ($wp_query->is_main_query()) && get_query_var( 'oni-daiko' ) ) {
-			restore_current_blog();
+		if ( !is_admin() && $wp_query->is_main_query() && get_query_var( $this->slug ) ) {
 			switch_to_blog( $post->blog_id );
 		}
 	}
 
 	public function add_loop_end( $query ) {
-		if ( !is_admin() && ($query->is_main_query()) && get_query_var( 'oni-daiko' ) ) {
-			restore_current_blog();
+		if ( !is_admin() && $query->is_main_query() && get_query_var( $this->slug ) ) {
+			switch_to_blog( 1 );
 		}
 	}
 
 	public function admin_menu() {
-		add_menu_page( __( 'Oni Daiko', $this->domain ), __( 'Oni Daiko', $this->domain ), 'manage_network', __FILE__, $this->setting_menu, $this->plugin_url . '/images/admin_side.gif' );
+		add_menu_page( __( 'Oni Daiko', $this->domain ), __( 'Oni Daiko', $this->domain ), 'manage_network', $this->domain, array( &$this, 'add_admin_edit_page' ), $this->plugin_url . '/admin/images/menu-icon.gif' );
 	}
 	
-	public function setting_menu() {
+	public function add_admin_edit_page() {
+		$title = __( 'Set Oni Daiko', $this->domain ); ?>
+		<div class="wrap">
+		<?php screen_icon(); ?>
+		<h2><?php echo esc_html( $title ); ?></h2>
+		<form method="post" action="options.php">
+		<?php settings_fields( $this->domain ); ?>
+		<?php do_settings_sections( $this->domain ); ?>
+		<input type="hidden" name="refresh">
+		<table class="form-table">
+		<?php do_settings_fields( $this->domain, 'default' ); ?>
+		</table>
+		<?php submit_button(); ?>
+		</form>
+		</div>
+	<?php }
+
+
+	public function add_general_custom_fields() {
+		add_settings_field( 'oni-daiko-main-include', __( 'Main sites include list.', $this->domain ), array( &$this, 'onid_check_box' ), $this->domain, 'default', array( 'name' => 'oni-daiko-main-include', 'value' => $this->main_include, 'note' => 'Enabling' ) );
+		add_settings_field( 'oni-daiko-slug', __( 'Oni Daiko slug setting', $this->domain ), array( &$this, 'onid_text_field' ), $this->domain, 'default', array( 'name' => 'oni-daiko-slug', 'value' => $this->slug ) );
 	}
-	
+
+	public function onid_check_box( $args ) {
+		extract( $args );
+		$output = '<label><input type="checkbox" name="' . $args['name'] .'" id="' . $args['name'] .'" value="1"' . checked( 1, $args['value'], false ). ' />' . esc_html__( $args['note'], $this->domain ) . '</label>' ."\n";
+		echo $output;
+	}
+
+	public function onid_text_field( $args ) {
+		extract( $args );
+		$output = '<label><input type="text" name="' . $args['name'] .'" id="' . $args['name'] .'" value="' . $args['value'] .'" /></label>' ."\n";
+		echo $output;
+	}
+
+	public function add_custom_whitelist_options_fields() {
+		register_setting( $this->domain, 'oni-daiko-main-include', 'intval' );
+		register_setting( $this->domain, 'oni-daiko-slug' );
+	}
+
+	public function admin_styles() {
+		wp_enqueue_style( 'admin-oni-daiko-style', $this->plugin_url . '/admin/css/style.css' );
+	}
+
 } // end of class
